b42430d0ee2d6b68a6e46bed5af1c94ee1e66011
CO17322 <36639198+CO17322@users.noreply.github.com>
2018-08-07 22:57:41 +0530

    For Loop
    For Loop in C language

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int counter;
+    for(counter=0;counter<=10;counter++)
+    {
+        printf("%d\n",counter);
+    }
+    getch();
+    return 0;
+}
